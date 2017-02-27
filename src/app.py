536ff733cd8265b624f8f1b66d331447f77ca870
gowthamand7 <gowthamand7@gmail.com>
2017-02-27 22:02:24 +0530

    completed the message pulling from google

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
+import uuid
+
+import flask
 from flask import Flask, render_template, request, url_for
-from flask import session
 from werkzeug.utils import redirect
 
-import src.common.utils as utils
 from src.models.google import Google
 
 app = Flask(__name__)
-app.secret_key = 'testing'
 
 
 @app.route('/')
@@ -20,18 +20,24 @@ def google_auth():
     flow = Google.get_flow()
     if request.args.get('code') is not None:
         credentials = Google.get_credentials(request.args.get('code'))
-        credentials_str = "accesstoken: {} \n Refreshtoken: {}".format(credentials.access_token,credentials.refresh_token)
-        user_info = Google.get_user_info(credentials)
-        user_info_str = utils.dist_to_string(user_info)
-        session['user_info'] = "here is the token details {} \n here is the user details {} \n".format(credentials_str,user_info_str)
+        flask.session['credentials'] = credentials.to_json()
         return redirect(url_for('home'))
     else:
         return redirect(url_for('index'))
 
 
+@app.route('/home/<string:next_page_token>')
 @app.route('/home')
-def home():
-    return session['user_info']
+def home(next_page_token= None):
+    google = Google(flask.session['credentials'])
+    user_info = google.get_user_info()
+    labels = google.get_labels()
+    if next_page_token is None:
+        messages, next_page_token = google.get_messages()
+    else:
+        messages, next_page_token = google.get_messages(page_token=next_page_token)
+    return render_template('inbox.html', labels=labels, user_info=user_info, messages=messages, nextPageToken=next_page_token)
 
 if __name__ == '__main__':
+    app.secret_key = str(uuid.uuid4())
     app.run(debug=True, use_debugger=False, use_reloader=False)
--- src/models/google.py
+++ src/models/google.py
@@ -1,9 +1,12 @@
+import datetime
 import os
 
+import flask
 import httplib2
-from flask import json
+from googleapiclient import discovery
 from googleapiclient import errors
 from googleapiclient.discovery import build
+from oauth2client import client
 from oauth2client.client import flow_from_clientsecrets
 
 from src import config
@@ -11,8 +14,52 @@ from src import config
 
 class Google(object):
 
-    def __init__(self):
-        pass
+    def __init__(self,json_credentials):
+        self.credentials = client.OAuth2Credentials.from_json(json_credentials)
+        self.mail_service = self.build_mail_service()
+        self.user_info = self.get_user_info()
+
+    def get_user_info(self):
+        user_info_service = build(
+            serviceName='oauth2', version='v2',
+            http=self.credentials.authorize(httplib2.Http()))
+        user_info = user_info_service.userinfo().get().execute()
+        return user_info
+
+    def get_labels(self):
+        labels = self.mail_service.users().labels().list(userId='me').execute()
+        return labels['labels']
+
+    def get_messages(self, page_token=None):
+        mails = self.mail_service.users().messages().list(userId='me',maxResults=20,pageToken=page_token, q='!in:chats').execute()
+        msg = []
+        for mail in mails['messages']:
+            metadata = self.mail_service.users().messages().get(userId='me', id=mail['id'], format='metadata').execute()
+            msg.append(Google.parse_metadata(metadata))
+        return msg, mails['nextPageToken']
+
+    @staticmethod
+    def parse_metadata(metadata):
+        id = metadata['id']
+        date = metadata['internalDate']
+        subject = None
+        from_email = None
+        for headers in metadata['payload']['headers']:
+            if headers['name'] == 'Subject':
+                subject = headers['value']
+            if headers['name'] == 'From':
+                from_email = headers['value']
+        return {
+            "id": id,
+            "date": datetime.datetime.fromtimestamp(int(date)/1e3).strftime('%Y-%m-%d %H:%M:%S'),
+            "subject": subject,
+            "from_email": from_email
+        }
+
+
+    def build_mail_service(self):
+        http_auth = self.credentials.authorize(httplib2.Http())
+        return discovery.build(serviceName='gmail', version='v1', http=http_auth)
 
     @staticmethod
     def get_auth_uri():
@@ -28,22 +75,8 @@ class Google(object):
     @staticmethod
     def get_credentials(code):
         flow = Google.get_flow()
-        return flow.step2_exchange(code)
-
-    @staticmethod
-    def get_user_info(credentials):
-        user_info_service = build(
-            serviceName='oauth2', version='v2',
-            http=credentials.authorize(httplib2.Http()))
-        user_info = None
-        try:
-            user_info = user_info_service.userinfo().get().execute()
-        except errors.HttpError as e:
-            if user_info and user_info.get('id'):
-                return user_info
-            else:
-                raise NoUserIdException()
-        return user_info
+        credentials = flow.step2_exchange(code)
+        return credentials
 
 
 class NoUserIdException(Exception):
