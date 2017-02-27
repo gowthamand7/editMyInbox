e2c31d1a0a594a3773ce5db83093720222a056ce
gowthamand7 <gowthamand7@gmail.com>
2017-02-27 18:12:02 +0530

    added redirction

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+from flask import session
+from werkzeug.utils import redirect
 
 import src.common.utils as utils
 from src.models.google import Google
@@ -8,7 +10,7 @@ app.secret_key = 'testing'
 
 
 @app.route('/')
-def home():
+def index():
     auth_uri = Google.get_auth_uri()
     return render_template('home.html', url=auth_uri)
 
@@ -21,10 +23,15 @@ def google_auth():
         credentials_str = "accesstoken: {} \n Refreshtoken: {}".format(credentials.access_token,credentials.refresh_token)
         user_info = Google.get_user_info(credentials)
         user_info_str = utils.dist_to_string(user_info)
-        return "here is the token details {} \n here is the user details {} \n".format(credentials_str,user_info_str)
+        session['user_info'] = "here is the token details {} \n here is the user details {} \n".format(credentials_str,user_info_str)
+        return redirect(url_for('home'))
     else:
-        auth_uri = flow.step1_get_authorize_url()
-        return render_template('home.html', url=auth_uri)
+        return redirect(url_for('index'))
+
+
+@app.route('/home')
+def home():
+    return session['user_info']
 
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False)
