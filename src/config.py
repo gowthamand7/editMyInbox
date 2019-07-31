cf5e683672f5986aece079099e625c1a906b91ca
Gowthaman Rooster <gowthamand7@gmail.com>
2019-07-31 13:13:12 +0530

    H

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-CLIENT_ID = '683973089673-9dfvapfnc4csqkh2dtjdtkat12mol8uq.apps.googleusercontent.com'
+CLIENT_ID s.googleusercontent.com'
 APPLICATION_NAME = 'Edit My Inbox'
 SCOPE = 'openid email https://mail.google.com/'
 redirect_uri = 'http://127.0.0.1:5000/google_auth'
